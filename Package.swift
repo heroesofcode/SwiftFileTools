0edb26b027e5429f7cd713e24e494ca91af9055b
joaolfp <joaolucasfp2001@gmail.com>
2023-11-20 10:06:11 -0300

    Adjust package

--- Package.swift
+++ Package.swift
@@ -5,17 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftFileTools",
+    platforms: [
+        .macOS(.v10_13)
+    ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "SwiftFileTools",
             dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
             ]
-        ),
+        )
     ]
 )
