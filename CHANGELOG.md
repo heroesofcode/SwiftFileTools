3ac09829fa115cc9b1f1b1ec4f8181b84ba83a19
joaolfp <joaolucasfp2001@gmail.com>
2023-12-17 12:14:35 -0300

    Bump version to 1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # CHANGELOG
 
+## Version 1.1.0
+**2023-12-17**
+
+- Updated swift-argument-parser to 1.3.0 (was 1.2.3)
+
 ## Version 1.0.0
 **2023-11-20**
 
--- Sources/Core/Header.swift
+++ Sources/Core/Header.swift
@@ -15,7 +15,7 @@ final class Header {
         Figlet.say("SwiftFileTools")
         
         print("About: With SwiftFileTools you can generate everyday files such as Colors, Constants, ViewCode and files for SwiftGen.".blue)
-        print("Version: 1.0.0".blue)
+        print("Version: 1.1.0".blue)
         print("Github: https://github.com/heroesofcode/SwiftFileTools".blue)
     }
 }
